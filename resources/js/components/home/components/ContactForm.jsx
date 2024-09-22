eca4ba954fdd702af4b312995ba6f7a555b35762
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-22 21:43:04 +0530

    Button background color changed

--- resources/js/components/home/components/ContactForm.jsx
+++ resources/js/components/home/components/ContactForm.jsx
@@ -114,7 +114,7 @@ const ContactForm = () => {
             <button
               type="submit"
               disabled={isSubmitting}
-              className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${isSubmitting ? "opacity-75 cursor-not-allowed" : ""}`}
+              className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-pink hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${isSubmitting ? "opacity-75 cursor-not-allowed" : ""}`}
             >
               {isSubmitting ? (
                 <>
