5162508a1cda6e10733c8dece969374f78b9ddbf
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-24 16:14:27 +0530

    Folder created for about page

---
+++ resources/js/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageBanner from '../common/PageBanner'
+import AboutBanner from 'imgPath/banners/about.png'
+
+function About() {
+    return (
+        <>
+        <PageBanner Banner = {AboutBanner} />
+        </>
+    )
+}
+
+export default About
