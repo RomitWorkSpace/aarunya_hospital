b92eea219713b677acab2438479f385dd6ace1d1
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-26 21:36:18 +0530

    Cursor pointer added

--- resources/js/components/common/ButtonRounded.jsx
+++ resources/js/components/common/ButtonRounded.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ButtonRounded({name}) {
     return (
         <>
-        <div className="bg-pink text-white inline-block py-2 px-4 rounded-full text-sm">
+        <div className="bg-pink text-white inline-block cursor-pointer py-2 px-4 rounded-full text-sm">
             {name}
         </div>
         </>
