7abc294de10dd23bf4543b45df4f1ee5566e9b32
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-26 21:43:55 +0530

    Home page hero section created

--- resources/js/components/home/components/HeroSection.jsx
+++ resources/js/components/home/components/HeroSection.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import Hero from 'imgPath/image.jpg'
+import HeroSlider from './HeroSlider'
 
 function HeroSection() {
     return (
         <>
-        <div className="container">
-            <img src={Hero} alt="Aarunya clinic" width="100%" />
-        </div>
+            <HeroSlider />
         <div className="container text-center px-3">
             <h1 className="clr-purple text-xl mt-10 font-semibold md:text-4xl">Welcome To Aarunya Clinic</h1>
             <p className="clr-purple md:text-lg">The Compassinate Health Care</p>
