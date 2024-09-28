b9c547f54393fab453622c429895ff8a7f485cbb
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-28 18:23:29 +0530

    Page banner added

--- resources/js/components/contact/Contact.jsx
+++ resources/js/components/contact/Contact.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import PageBanner from '../common/PageBanner'
 
-import ContactBanner from 'imgPath/banners/contact.png'
+import DkBanner from 'imgPath/banners/dk-contact.jpg'
+import MbBanner from 'imgPath/banners/mb-contact.jpg'
 import ContactSection from '../home/components/ContactSection'
 import InfoSection from '../home/components/InfoSection'
 
 function Contact() {
     return (
         <>
-        <PageBanner Banner = {ContactBanner} />
+        <PageBanner Banner = {DkBanner} MobileBanner = {MbBanner} />
         <ContactSection />
         <InfoSection />
         </>
