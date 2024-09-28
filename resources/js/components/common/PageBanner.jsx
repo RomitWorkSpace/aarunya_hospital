5517454e4c075b96a9c26348aae4159753b1e7b2
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-28 18:22:05 +0530

    Responsive page banner

--- resources/js/components/common/PageBanner.jsx
+++ resources/js/components/common/PageBanner.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
-function PageBanner({Banner}) {
-    return (
-        <>
-        <div className="container relative">
-            <img src={ Banner } alt="banner" width="100%" />
-        </div>
-        </>
-    )
+function PageBanner({Banner, MobileBanner}) {
+    const isMobileSlider = window.matchMedia('(max-width: 768px)').matches;
+    if (isMobileSlider){
+        return ( 
+            <>
+            <div className="container relative">
+                <img src={ MobileBanner } alt="banner" width="100%" />
+            </div>
+            </>
+        );
+    }else {
+        return ( 
+            <>
+            <div className="container relative">
+                <img src={ Banner } alt="banner" width="100%" />
+            </div>
+            </>
+        );
+    }
 }
 
 export default PageBanner
